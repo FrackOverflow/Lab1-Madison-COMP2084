4d74fc3f429a976b875641f525386d09ea00983c
FrackOverflow <madison.hd.pardy@gmail.com>
2022-05-11 21:40:35 -0300

    Add project files.

---
+++ Lab1-Madison-COMP2084/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab1_Madison_COMP2084.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
